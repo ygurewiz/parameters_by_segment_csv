2837d95138ec4accb4bd9b5dc3d9d407039b6de2
ygurewiz <ygurewiz@ecoppia.com>
2020-12-09 22:04:23 +0200

    starter

--- parameters_by_segment_csv.py
+++ parameters_by_segment_csv.py
@@ -2,20 +2,35 @@ import sys
 import csv
 import json
 
-def createJsonFile(robotSegmentsList,newJsonFile):
-    for line in newJsonFile.readlines()
-        if l in robotSegmentsList:
-
+def createJsonFileLines(robotSegmentsList,jsonFile,newJsonFile):
+    data = json.load(jsonFile)
+    res = {}
+    for param in data:
+        if(param=='surface_map'):
+            continue
+        if(param=='ProtocolVersion'):
+            continue
+        res[param]=[]
+        res[param].append(data[param])
+    newJsonFile.write('{')
+    for r in res:
+        newJsonFile.write('\t')
+        json.dump(r, newJsonFile)
+        newJsonFile.write(':')
+        json.dump(res[r][0], newJsonFile)
+        newJsonFile.write(',\n')
+    newJsonFile.write('}')
+    
 
 
 def parseLines(csvSurfacesFileName,robotParamsFileName):
+    i=0
+    numRobots = 0
     with  open(csvSurfacesFileName, newline='') as csvSurfacesFile:
         csvReader = csv.DictReader(csvSurfacesFile)
         jsonFile = open(robotParamsFileName)
         currentRobot = ''
         lastRobot = ''
-        numRobots = 0
-        i=0
         robotSegmentsList = list()
         for line in csvReader:
             i+=1
@@ -29,18 +44,24 @@ def parseLines(csvSurfacesFileName,robotParamsFileName):
                     lastRobot = currentRobot
                     currentRobot = ''
                     robotSegmentsList = list()
-            newJsonFile = open(currentRobot+'json','+w')
-            #res = createJsonFile(robotSegmentsList,newJsonFile)
-
+            newJsonFile = open(currentRobot+'.json','+w')
+            createJsonFileLines(robotSegmentsList,jsonFile,newJsonFile)
+            jsonFile.close()
+            newJsonFile.close()
+            if(i==1):
+                break
             print(i)
+
     csvSurfacesFile.close()
+    return i,numRobots
 
 def main(argv):
     if(len(argv)<3):
         print("error in input")
         return
-    res= parseLines(argv[1],argv[2])
-    print(res)
+    numRows,numRobots= parseLines(argv[1],argv[2])
+    print('number of rows parsed = '+str(numRows))
+    print('number of robot files created = '+str(numRobots))
 
 if __name__=="__main__":
     main(sys.argv)
