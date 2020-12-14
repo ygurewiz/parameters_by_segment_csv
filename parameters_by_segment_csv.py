2ffb89b7dd4dfa67cde2e52c92bb67a30bcfee48
ygurewiz <ygurewiz@ecoppia.com>
2020-12-14 17:54:58 +0200

    done done done

--- parameters_by_segment_csv.py
+++ parameters_by_segment_csv.py
@@ -1,67 +1,225 @@
 import sys
 import csv
 import json
+import str
+import string
 
-def createJsonFileLines(robotSegmentsList,jsonFile,newJsonFile):
-    data = json.load(jsonFile)
+def createJsonFileLines(robotSegmentsList,data,newJsonFile,width):
+
+    res = createJsonData(data,robotSegmentsList,width)
+
+    addLineToJson(True,'{\n',res,newJsonFile,False)
+    length = 0
+    p = len(res)
+    for headToken in res:
+        length +=1
+        addLineToJson(False,headToken,'',newJsonFile,False)
+        if(length==p):
+            addLineToJson(False,headToken,res,newJsonFile,True)
+        else:
+            addLineToJson(False,headToken,res,newJsonFile,False)
+        #if(not length==p):
+         #   newJsonFile.write(',')
+    addLineToJson(True,'}\n',res,newJsonFile,True)
+ 
+    
+
+def addLineToJsonList(tokensList,newJsonFile,islast):    
+    l = len(tokensList)
+    if(l>1):
+        i=0
+        newJsonFile.write('[\n')
+        for line in tokensList:
+            i+=1
+            newJsonFile.write('{\n')
+            k = 0
+            for token in line:
+                k+=1
+                json.dump(token, newJsonFile)
+                newJsonFile.write(': ')
+                json.dump(line[token], newJsonFile)
+                if(k==len(line)):
+                    newJsonFile.write('\n')
+                else:
+                    newJsonFile.write(',\n')
+            if(i==l):
+                newJsonFile.write('}\n]')
+            else:
+                newJsonFile.write('},\n')
+        if (not islast):
+            newJsonFile.write(',\n')
+        return
+    else:
+        if(type(tokensList[0])==dict):
+            i=0
+            theDict = tokensList[0]
+            newJsonFile.write('{\n')
+            for t in theDict:
+                i+=1
+                json.dump(t, newJsonFile)
+                newJsonFile.write(' : ')
+                json.dump(theDict[t], newJsonFile)
+                if(not i==len(theDict)):
+                    newJsonFile.write(',\n')
+                else:
+                    newJsonFile.write('\n}')
+        else:
+            json.dump(tokensList[0], newJsonFile)
+        if(not islast):
+            newJsonFile.write(',\n')
+        else:
+            newJsonFile.write('\n')
+        return
+
+def addLineToJson(istoken,headToken,res,newJsonFile,islast):
+    if(istoken):
+        newJsonFile.write(headToken)
+        return
+    if(res==''):
+        json.dump(headToken, newJsonFile)
+        newJsonFile.write(': ')
+        return
+    if(type(res[headToken])==list):
+        addLineToJsonList(res[headToken],newJsonFile,islast)
+    else:
+        print(headToken)
+
+
+    
+
+def createJsonData(data,robotSegmentsList,width):
     res = {}
     for param in data:
         if(param=='surface_map'):
-            continue
-        if(param=='ProtocolVersion'):
-            continue
-        res[param]=[]
-        res[param].append(data[param])
-    newJsonFile.write('{')
-    for r in res:
-        newJsonFile.write('\t')
-        json.dump(r, newJsonFile)
-        newJsonFile.write(':')
-        json.dump(res[r][0], newJsonFile)
-        newJsonFile.write(',\n')
-    newJsonFile.write('}')
-    
+            res[param]=[]
+            surfaceJsonData = data[param]
+            p = len(robotSegmentsList)
+            for i in range(len(surfaceJsonData)):
+                if(i>len(robotSegmentsList)-1):
+                        surfaceJsonData[i]['type']=0
+                        res[param].append(surfaceJsonData[i])
+                        continue
+                surfaceDict = robotSegmentsList[i]
+      
+                if(surfaceDict["Entity"]=='Tracker'):
+                    surfaceJsonData[i]['type']=3
+                    surfaceJsonData[i]['length']=int(int(surfaceDict['Area'])*1000/int(width))
 
+                elif(surfaceDict["Entity"]=='Bridge'):
+                    surfaceJsonData[i]['type']=2
+                    if('-' in surfaceDict['Gap']):
+                        gaps = surfaceDict['Gap'].split('-')
+                        gap = max(int(gaps[0]),int(gaps[1]))
+                        surfaceJsonData[i]['length']=gap*10
+                    elif('<' in surfaceDict['Gap']):
+                        gaps = surfaceDict['Gap'].split('<')
+                        gap = int(gaps[1])
+                        surfaceJsonData[i]['length']=gap*10
+                    elif('>' in surfaceDict['Gap']):
+                        gaps = surfaceDict['Gap'].split('>')
+                        gap = int(gaps[1])
+                        surfaceJsonData[i]['length']=gap*10
+                    else:
+                        print("NOOO!!")
 
-def parseLines(csvSurfacesFileName,robotParamsFileName):
+                elif(surfaceDict["Entity"]=='Dock'):
+                    surfaceJsonData[i]['type']=1
+                    if(surfaceDict['Gap']=='Edge'):
+                        surfaceJsonData[i]['length']=750
+                        parkingType = surfaceDict['Row'].split('-')[2]
+                        if(parkingType=='01'):
+                            data["parking_side"]=1
+                        elif(parkingType=='02' or parkingType=='03'):
+                            data["parking_side"]=0
+                        else:
+                            print("NOOO!!")
+                    else:
+                        if('-' in surfaceDict['Gap']):
+                            gaps = surfaceDict['Gap'].split('-')
+                            gap = max(int(gaps[0]),int(gaps[1]))
+                            surfaceJsonData[i]['length']=gap*10
+                        elif('<' in surfaceDict['Gap']):
+                            gaps = surfaceDict['Gap'].split('<')
+                            gap = int(gaps[1])
+                            surfaceJsonData[i]['length']=gap*10
+                        elif('>' in surfaceDict['Gap']):
+                            gaps = surfaceDict['Gap'].split('>')
+                            gap = int(gaps[1])
+                            surfaceJsonData[i]['length']=gap*10
+                        else:
+                            print("NOOO!!")
+
+                elif(surfaceDict["Entity"]=='Gap'):
+                    surfaceJsonData[i]['type']=0
+                    gaps = surfaceDict['Gap'].split('-')
+                    gap = max(int(gaps[0]),int(gaps[1]))
+                    surfaceJsonData[i]['length']=gap*10
+
+                res[param].append(surfaceJsonData[i])  
+                #print(param)
+        else:
+            res[param]=[]
+            res[param].append(data[param])
+            #print(param)
+    return res
+
+def parseLines(csvSurfacesFileName,robotParamsFileName,width):
     i=0
     numRobots = 0
     with  open(csvSurfacesFileName, newline='') as csvSurfacesFile:
         csvReader = csv.DictReader(csvSurfacesFile)
         jsonFile = open(robotParamsFileName)
+        data = json.load(jsonFile)
+
         currentRobot = ''
-        lastRobot = ''
+        newRobot = True
         robotSegmentsList = list()
+        doneList = robotSegmentsList
+        doneRobot = currentRobot
         for line in csvReader:
             i+=1
-            if(currentRobot==''):
+            #if(line['Row']=='E-077-01'):
+             #   print('0')
+            if(newRobot):
                 currentRobot=line['Row']
-                numRobots+=1
+                newRobot = False
                 robotSegmentsList.append(line)
                 print(currentRobot)
             else:
                 if(not line['Row']==currentRobot):
-                    lastRobot = currentRobot
-                    currentRobot = ''
+                    doneRobot = currentRobot
+                    currentRobot = line['Row']
+                    newRobot = True
+                    doneList = robotSegmentsList
                     robotSegmentsList = list()
-            newJsonFile = open(currentRobot+'.json','+w')
-            createJsonFileLines(robotSegmentsList,jsonFile,newJsonFile)
-            jsonFile.close()
-            newJsonFile.close()
-            if(i==1):
-                break
-            print(i)
+                    numRobots+=1
 
+                    newJsonFile = open(doneRobot+'.json','+w')
+                    createJsonFileLines(doneList,data,newJsonFile,width)
+                    newJsonFile.close()
+                    #print(i)
+                
+                robotSegmentsList.append(line)
+                #print(line)
+            #if(numRobots==2):
+            #    break
+        newJsonFile = open(currentRobot+'.json','+w')
+        createJsonFileLines(robotSegmentsList,data,newJsonFile,width)
+        newJsonFile.close()
+
+        
+
+    jsonFile.close()
     csvSurfacesFile.close()
     return i,numRobots
 
 def main(argv):
-    if(len(argv)<3):
+    if(len(argv)<4):
         print("error in input")
         return
-    numRows,numRobots= parseLines(argv[1],argv[2])
-    print('number of rows parsed = '+str(numRows))
-    print('number of robot files created = '+str(numRobots))
+    width = argv[3]
+    numRows,numRobots= parseLines(argv[1],argv[2],argv[3])
+    print("number of rows parsed = {} and number of robot files created = {}".format(numRows, numRobots))
 
 if __name__=="__main__":
     main(sys.argv)
